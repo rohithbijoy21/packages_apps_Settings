DirtyUnicorns/android_packages_apps_DU-Tweaks

Navigation button transparency (2/3)
Change-Id: I167d99aed101639f98d31e80384fddcd0f9cbf5b

src/com/dirtyunicorns/dutweaks/fragments/SmartbarSettings.java
@@ -59,12 +59,14 @@ import com.android.internal.utils.du.Config;
 import com.android.internal.utils.du.Config.ButtonConfig;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.dirtyunicorns.dutweaks.widget.SeekBarPreferenceCham;
 
 public class SmartbarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     private ListPreference mSmartBarContext;
     private ListPreference mImeActions;
     private ListPreference mButtonAnim;
+    private SeekBarPreferenceCham mButtonsAlpha;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int MENU_SAVE = Menu.FIRST + 1;
@@ -79,6 +81,7 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
     private static final String SMARTBAR_CONFIGS_PREFIX = "smartbar_config_";
     private static final String KEY_SMARTBAR_BACKUP = "smartbar_profile_save";
     private static final String KEY_SMARTBAR_RESTORE = "smartbar_profile_restore";
+    private static final String PREF_NAVBAR_BUTTONS_ALPHA = "navbar_buttons_alpha";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -102,6 +105,13 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
         mButtonAnim = (ListPreference) findPreference("smartbar_button_animation");
         mButtonAnim.setValue(String.valueOf(buttonAnimVal));
         mButtonAnim.setOnPreferenceChangeListener(this);
+
+        mButtonsAlpha =
+                (SeekBarPreferenceCham) findPreference(PREF_NAVBAR_BUTTONS_ALPHA);
+        int bAlpha = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.NAVBAR_BUTTONS_ALPHA, 255, UserHandle.USER_CURRENT);
+        mButtonsAlpha.setValue(bAlpha / 1);
+        mButtonsAlpha.setOnPreferenceChangeListener(this);
 
         setHasOptionsMenu(true);
     }
@@ -234,6 +244,11 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(getContentResolver(), "smartbar_ime_hint_mode",
                     val);
             return true;
+        } else if (preference == mButtonsAlpha) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.NAVBAR_BUTTONS_ALPHA, val * 1, UserHandle.USER_CURRENT);
+            return true;
         }
         return false;
     }
@@ -263,6 +278,11 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
                 "smartbar_button_animation_style", 0);
         mButtonAnim.setValue(String.valueOf(0));
         mButtonAnim.setOnPreferenceChangeListener(this);
+
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                "navbar_buttons_alpha", 255);
+        mButtonsAlpha.setValue(255);
+        mButtonsAlpha.setOnPreferenceChangeListener(this);
     }
 
     static class ConfigAdapter extends ArrayAdapter<File> {

rohithbijoy21/packages_apps_Settings

src/com/android/settings/custom/nav/SmartbarSettings.java
@@ -59,12 +59,14 @@ import com.android.internal.utils.du.Config;
 import com.android.internal.utils.du.Config.ButtonConfig;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.widget.SeekBarPreferenceCham;
 
 public class SmartbarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     private ListPreference mSmartBarContext;
     private ListPreference mImeActions;
     private ListPreference mButtonAnim;
+    private SeekBarPreferenceCham mButtonsAlpha;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int MENU_SAVE = Menu.FIRST + 1;
@@ -79,6 +81,7 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
     private static final String SMARTBAR_CONFIGS_PREFIX = "smartbar_config_";
     private static final String KEY_SMARTBAR_BACKUP = "smartbar_profile_save";
     private static final String KEY_SMARTBAR_RESTORE = "smartbar_profile_restore";
+    private static final String PREF_NAVBAR_BUTTONS_ALPHA = "navbar_buttons_alpha";
 
     Context mContext;
 
@@ -105,6 +108,13 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
         mButtonAnim = (ListPreference) findPreference("smartbar_button_animation");
         mButtonAnim.setValue(String.valueOf(buttonAnimVal));
         mButtonAnim.setOnPreferenceChangeListener(this);
+
+        mButtonsAlpha =
+                (SeekBarPreferenceCham) findPreference(PREF_NAVBAR_BUTTONS_ALPHA);
+        int bAlpha = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.NAVBAR_BUTTONS_ALPHA, 255, UserHandle.USER_CURRENT);
+        mButtonsAlpha.setValue(bAlpha / 1);
+        mButtonsAlpha.setOnPreferenceChangeListener(this);
 
         setHasOptionsMenu(true);
     }
@@ -237,6 +247,11 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(getContentResolver(), "smartbar_ime_hint_mode",
                     val);
             return true;
+        } else if (preference == mButtonsAlpha) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.NAVBAR_BUTTONS_ALPHA, val * 1, UserHandle.USER_CURRENT);
+            return true;
         }
         return false;
     }
@@ -266,6 +281,11 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
                 "smartbar_button_animation_style", 0);
         mButtonAnim.setValue(String.valueOf(0));
         mButtonAnim.setOnPreferenceChangeListener(this);
+
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                "navbar_buttons_alpha", 255);
+        mButtonsAlpha.setValue(255);
+        mButtonsAlpha.setOnPreferenceChangeListener(this);
     }
 
     static class ConfigAdapter extends ArrayAdapter<File> {