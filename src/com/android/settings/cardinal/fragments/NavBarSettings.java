1b7039db7a0374b3d05efc32f238f0a150a98429
Adarsh-MR <adarshmr1998@gmail.com>
2017-06-16 18:43:03 +0000

    Settings: Wings: move some stuff to NavigationBar
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- src/com/android/settings/cardinal/fragments/NavBarSettings.java
+++ src/com/android/settings/cardinal/fragments/NavBarSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings;
+package com.android.settings.cardinal.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
