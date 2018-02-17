shivatejapeddi/packages_apps_Settings-1

Settings: Add activity for expanded desktop
* also add missing stuff into SETTINGS_FOR_RESTRICTED

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,7 @@ import com.citrus.settings.fragments.PulseSettings;
 import com.citrus.settings.fragments.SmartbarSettings;
 import com.citrus.settings.fragments.QuickSettings;
 import com.citrus.settings.fragments.NotificationStyleSettings;
+import com.citrus.settings.fragments.ExpandedDesktop;
 import com.android.settings.custom.DisplayRotation;
 
 public class SettingsGateway {
@@ -269,7 +270,8 @@ public class SettingsGateway {
             PulseSettings.class.getName(),
             QuickSettings.class.getName(),
             DisplayRotation.class.getName(),
-            NotificationStyleSettings.class.getName()
+            NotificationStyleSettings.class.getName(),
+            ExpandedDesktop.class.getName()            
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -311,6 +313,11 @@ public class SettingsGateway {
             Settings.NavigationSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
-            Settings.PulseSettingsActivity.class.getName()
+            Settings.PulseSettingsActivity.class.getName(),
+            Settings.QuickSettingsActivity.class.getName(),     
+            Settings.RotationSettingsActivity.class.getName(),      
+            Settings.NotificationStyleSettingsActivity.class.getName(),
+            Settings.OmniJawsSettingsActivity.class.getName(),       
+            Settings.ExpandedDesktopSettingsActivity.class.getName()               
     };
 }

rohithbijoy21/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.cardinal.settings.fragments.NotificationStyleSettings;
 import com.cardinal.settings.fragments.QuickSettings;
 import com.cardinal.settings.fragments.NavigationBarSettings;
 import com.cardinal.settings.fragments.NotificationSettings;
+import com.cardinal.settings.fragments.ExpandedDesktop;
 import com.android.settings.custom.DisplayRotation;
 
 public class SettingsGateway {
@@ -265,7 +266,8 @@ public class SettingsGateway {
             QuickSettings.class.getName(),
             NotificationStyleSettings.class.getName(),
             NotificationSettings.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+            ExpandedDesktop.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -305,5 +307,6 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.CardinalSettingsActivity.class.getName(),
+            Settings.ExpandedDesktopSettingsActivity.class.getName(),
     };
 }